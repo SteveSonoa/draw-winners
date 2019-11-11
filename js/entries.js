d8c416129ee834d597ad5092c1d80a9696046d7a
Steve Marshall <stevesonoa@gmail.com>
2019-11-10 17:18:51 -0700

    Added ability to draw random winners from a list one time. Updated basic styling.

---
+++ js/entries.js
@@ -0,0 +1,10 @@
+var contestName = 'RideTheTeacups.com YouTube Launch Party';
+
+var contestDate = 'November 9, 2019';
+
+var entries = [
+    'Robert Eng',
+    'Ravi S',
+    'Theresa Eng',
+    'Ravi S',
+];
